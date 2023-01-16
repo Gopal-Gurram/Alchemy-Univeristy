ccd175152103337075aafaf3c90ca4d8d39b94eb
Gopal-Gurram <gopalgurram1@gmail.com>
2023-01-16 21:55:14 +0530

    initial commit

---
+++ .history/server/scripts/generate_20230116202420.js
@@ -0,0 +1,6 @@
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography/utils");
+
+const privateK̥̥̥̥̥̥ey = secp.utils.randomPrivateKey();
+
+console.log("privateKey: ", toHex(privateK̥̥̥̥̥̥ey));
