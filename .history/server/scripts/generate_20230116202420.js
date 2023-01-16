d02dd6438f1f889604f8ff8174bedd1b8bef90be
Gopal Gurram <59303056+Gopal-Gurram@users.noreply.github.com>
2023-01-16 21:57:01 +0530

    Delete .history directory

--- .history/server/scripts/generate_20230116202420.js
+++
@@ -1,6 +0,0 @@
-const secp = require("ethereum-cryptography/secp256k1");
-const { toHex } = require("ethereum-cryptography/utils");
-
-const privateK̥̥̥̥̥̥ey = secp.utils.randomPrivateKey();
-
-console.log("privateKey: ", toHex(privateK̥̥̥̥̥̥ey));
